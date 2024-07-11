fc8378ab2f8501c0fa0efc37be6351b24e591a33
jessi-garcia <Tcheimportacoes@gmail.com>
2024-07-11 12:07:05 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,4 +15,21 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 1000);
     });
 });
+function comprarPeloWhatsApp(produto) {
+    var numero = '51985907028'; // Substitua pelo seu número de telefone
+    var mensagem = 'Olá, estou interessado no ' + Meia calça;
+    var url = 'https://wa.me/' + numero + '?text=' + encodeURIComponent(mensagem);
+    window.open(url, '_blank');
+}
 
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var city = document.getElementById('city').value;
+    var mailtoLink = 'tcheimportacoes@gmail.com?subject=Contato&body=' +
+        'Nome: ' + encodeURIComponent(name) + '%0A' +
+        'Email: ' + encodeURIComponent(email) + '%0A' +
+        'Cidade: ' + encodeURIComponent(city);
+    window.location.href = mailtoLink;
+});
